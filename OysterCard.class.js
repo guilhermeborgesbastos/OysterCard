08670c6520aa544426596ab2d6bddaac7fd114a4
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-29 13:46:35 -0200

    Setted the CONST vars

--- OysterCard.class.js
+++ OysterCard.class.js
@@ -9,6 +9,24 @@
  * @date       15/11/2017
 */
 
+"use strict";
+
+// const variables
+const BUS_COST = 1.8,
+      MAX_COST = 3.2,
+      COST_ONLY_ZONE_ONE = 2.5,
+      COST_ONE_ZONE_NOT_INCLUDING_ZONE_ONE = 2.0,
+      COST_TWO_ZONES_INCLUDING_ZONE_ONE = 3.0,
+      COST_TWO_ZONES_EXCLUDING_ZONE_ONE = 2.25;
+
+const STATIONS = {
+    Holborn: [1],
+    EarlsCourt: [1, 2],
+    Hammersmith: [2],
+    Wimbledon: [3]
+};
+
+
 class OysterCard {
 
 }
