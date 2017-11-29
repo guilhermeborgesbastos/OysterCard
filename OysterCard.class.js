91e364a29ae22f23e5f4c2e4323e209c8a455299
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-29 11:14:20 -0200

    Main class created
    Main class created, still empty.

---
+++ OysterCard.class.js
@@ -0,0 +1,14 @@
+/*
+ * OysterCard.class.js
+ *
+ * Class to manage the Oyster Card
+ *
+ * @version    0.1.0
+ * @author     Guilherme Borges Bastos
+ * @copyright  Copyright 2017 Guilherme Borges Bastos
+ * @date       15/11/2017
+*/
+
+class OysterCard {
+
+}
