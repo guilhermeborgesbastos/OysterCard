61915b06ad5c6199e550349a05848de63d4a2c37
Guilherme <guilhermeborgesbastos@gmail.com>
2017-11-29 13:59:17 -0200

    Test Driven Development Class Created

---
+++ OysterCard.test.js
@@ -0,0 +1,17 @@
+/*
+ * OysterCards.test.php
+ *
+ * Test Driven Development Class
+ * @version    0.1.0
+ * @author     Guilherme Borges Bastos
+ * @copyright  Copyright 2017 Guilherme Borges Bastos
+ * @LinkedIn   https://www.linkedin.com/in/guilhermeborgesbastos/
+ * 
+*/
+
+const OysterCard = require('./OysterCard.class');
+let card = new OysterCard();
+
+// #########################################
+// #### TODO: implement test's methods #####
+// #########################################
